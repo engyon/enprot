7ed836bed3ba3094e64b6c4052cff19e2361d86e
Daniel Wyatt <daniel.wyatt@ribose.com>
2020-02-04 12:31:35 -0500

    ci: Publish to crates.io.

--- ci/build-static.ps1
+++ ci/build-static.ps1
@@ -7,10 +7,6 @@ $BOTAN_MODULES = "$($(get-content 'ci\botan-modules') -join ',')"
   $ErrorActionPreference = 'Continue'
   & cargo install --version "$Env:CROSS_VERSION" cross
 }
-# update version in manifest to match our tag
-(Get-Content Cargo.toml) |
-  %{ $_ -replace '^version.*',"version = `"$Env:RELEASE_TAG`"" } |
-  Set-Content .\Cargo.toml
 
 # build
 &{
